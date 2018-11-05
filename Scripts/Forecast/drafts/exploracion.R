173d5f6c211dbacc517a3676710dd9974b72af02
DanielRZapataS <danielr.zapata@urosario.edu.co>
2018-11-05 18:57:21 -0500

    mejorar las graficas del script de exploración

--- Scripts/Forecast/drafts/exploracion.R
+++ Scripts/Forecast/drafts/exploracion.R
@@ -11,12 +11,19 @@ total <- length(offices)
 #Meta
 holidays <- get.path(meta_path, "holiday_dummy") %>% fread()
 paydays <- get.path(meta_path, "payday_dummy") %>% fread()
-holidays[ , fecha := as.Date(fecha, "%d/%m/%Y")]
-paydays[,  fecha := as.Date(fecha, "%d/%m/%Y")] 
+taxes <- get.path(meta_path, "taxes_dummy") %>% fread()
 names(holidays) <- toupper(names(holidays))
 names(paydays) <- toupper(names(paydays))
+names(taxes) <- toupper(names(taxes))
+setnames(holidays, "TYPE", "HOLIDAY_TYPE")
+setnames(paydays, "TYPE", "PAYDAY_TYPE")
+setnames(taxes, "TYPE", "TAXE_TYPE")
 
-dataset <- staging[codigoOficina == offices[3],
+holidays[ , FECHA := as.Date(FECHA, "%Y-%m-%d")]
+paydays[,  FECHA := as.Date(FECHA, "%d/%m/%Y")] 
+taxes[,  FECHA := as.Date(FECHA, "%Y-%m-%d")] 
+
+dataset <- staging[codigoOficina == offices[2],
                    .(TXS = as.numeric(TXS), FECHA)] 
 dataset[, ':='(DAY = day(FECHA),
                MES = month(FECHA),
@@ -37,32 +44,83 @@ dataset <- merge(dataset, calendar,
 
 dataset <- merge(dataset, holidays, by = "FECHA") 
 dataset <- merge(dataset, paydays, by = "FECHA")
+dataset <- merge(dataset, taxes, by = "FECHA")
 dataset[,WEEKDAY := wday(FECHA)] 
 
 
 dataset <- dataset[!(WEEKDAY %in% c(7,1)) ,]
 # 
-# #filtro de kalman solo para los dÃ?as que no son festivos
+# #filtro de kalman solo para los dias que no son festivos
 # dataset[HOLIDAYS == 0, TXS := na.interpolation(TXS)] 
-# # dÃ?as festivos iguales a cero en la serie
+# # dias festivos iguales a cero en la serie
 # dataset[HOLIDAYS == 1,TXS :=0 ]
+dataset <- dataset[HOLIDAYS == 0]
+dataset[, ':='(TXS_LAG1 = shift(TXS, 1, NA, "lag"))]
+dataset[, TXS_DIFF1 := TXS - TXS_LAG1]
+dataset[, ':='(TXS_DIFF1_LAG = shift(TXS_DIFF1, 1, NA, "lag"))]
+dataset[, TXS_DIFF2 := TXS_DIFF1 - TXS_DIFF1_LAG]
+
+dataset[, .(FECHA,
+            TXS,
+            TXS_LAG1,
+            TXS_DIFF1,
+            TXS_DIFF1_LAG,
+            TXS_DIFF2)]
 
 
-plot <- dataset[, .(FECHA, TXS, HOLIDAYS, PAYDAY)]
-zeroDays <- plot[HOLIDAYS == 1 , FECHA]
+plot <- dataset[, .(FECHA, TXS_DIFF1)]
+holiDays <- holidays[HOLIDAYS == 1, FECHA]
+payDays <- paydays[PAYDAY == 1 , FECHA]
+taxeDays <- taxes[TAXE_DAY == 1 , FECHA]
 
-ggplot(plot[HOLIDAYS == 0], aes(FECHA, TXS) )+
+mean <- mean(plot$TXS_DIFF1, na.rm = T)
+sd <- sd(plot$TXS_DIFF1, na.rm = T)
+
+
+
+
+plot1 <- ggplot(plot, aes(FECHA, TXS_DIFF1) )+
   geom_line(size = 1, alpha = 1)+
-  geom_vline(
-    xintercept = as.numeric(zeroDays),
+  geom_hline(
+    yintercept = c(mean, mean + 2*sd, mean - 2*sd,
+                   mean + sd, mean - sd),
     linetype = 1,
     size = 0.5,
     alpha = 0.5,
-    color = "green"
+    color = "blue"
+  )+
+  geom_vline(data = taxes[TAXE_DAY == 1],
+             aes(xintercept = as.numeric(FECHA),
+                 color = TAXE_TYPE
+                 )
+             )
+
+  # geom_vline(
+  #   xintercept = as.numeric(holidays[HOLIDAYS == 1, FECHA]),
+  #   linetype = 1,
+  #   size = 0.5,
+  #   alpha = 0.5,
+  #   color = HOLIDAY_TYPE
+  # )
+plot1 <- ggplotly(plot1)
+plot1
+
+plot2 <- ggplot(plot, aes(FECHA, TXS_DIFF1) )+
+  geom_line(size = 1, alpha = 1)+
+  geom_hline(
+    yintercept = c(mean, mean + 2*sd, mean - 2*sd,
+                   mean + sd, mean - sd),
+    linetype = 1,
+    size = 0.5,
+    alpha = 0.5,
+    color = "blue"
+  )+
+  geom_vline(data = holidays[HOLIDAYS == 1],
+             aes(xintercept = as.numeric(FECHA),
+                 color = HOLIDAY_TYPE
+             )
   )
 
-dataset[TXS == 0 & HOLIDAYS == 0]
-## diff
 
-diffBase <- dataset[, .(FECHA, TXS, HOLIDAYS)]
-txs_ts <- ts(diffBase$TXS, start = c(2016, 1,1), end = , frequency = 365)
+plot2 <- ggplotly(plot2)
+plot2
